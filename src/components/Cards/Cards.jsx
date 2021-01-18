5732c6166615a73f1323c4423c9ef87b2946950a
Marc <marcelopgm65@gmail.com>
2021-01-18 16:16:03 -0300

    :sparkles: Done!

---
+++ src/components/Cards/Cards.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+
+import { Grid } from '@material-ui/core';
+import CardComponent from './Card/Card';
+
+import styles from './Cards.module.css';
+
+const Info = ({ data: { confirmed, recovered, deaths, lastUpdate } }) => {
+  if (!confirmed) {
+    return 'Carregando...';
+  }
+
+  return (
+    <div className={styles.container}>
+      <Grid container spacing={3} justify="center">
+        <CardComponent
+          className={styles.infected}
+          cardTitle="Infectados"
+          value={confirmed.value}
+          lastUpdate={lastUpdate}
+          cardSubtitle="Número de casos ativos de COVID-19."
+        />
+        <CardComponent
+          className={styles.recovered}
+          cardTitle="Recuperados"
+          value={recovered.value}
+          lastUpdate={lastUpdate}
+          cardSubtitle="Número de recuperados do COVID-19."
+        />
+        <CardComponent
+          className={styles.deaths}
+          cardTitle="Mortes"
+          value={deaths.value}
+          lastUpdate={lastUpdate}
+          cardSubtitle="Número de mortes causadas por COVID-19."
+        />
+      </Grid>
+    </div>
+  );
+};
+
+export default Info;
